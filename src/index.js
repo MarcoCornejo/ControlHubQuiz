d04f823bd6390fae5d6b69cc2acf852ed7c8591a
Marco Cornejo <alejandroc_89@hotmail.com>
2021-03-19 15:31:03 -0700

    Created working express app

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log( "I'm running!" );
+const express = require("express");
+const app = express();
+
+const startApp = require('./app');
+
+app.listen(3000, () => {
+ console.log("Server running on port 3000");
+ startApp(app);
+});

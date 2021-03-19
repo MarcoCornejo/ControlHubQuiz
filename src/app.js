91bcbbedd5aead329722a22e8f32c5338148cc7e
Marco Cornejo <alejandroc_89@hotmail.com>
2021-03-19 16:20:37 -0700

    Implemented getFibNumber function

--- src/app.js
+++ src/app.js
@@ -2,7 +2,22 @@
 module.exports = (app) => {
     app.get("/getFib/:fibIndex", (req, res, next) => {
         const fibIndex = parseInt(req.params.fibIndex);
-        if(isNaN(fibIndex)) { return res.json('Not a number') };
-        res.json('Hello there, your entered number is: ' + req.params.fibIndex);
+        if(isNaN(fibIndex)) return res.json('Not a number');
+
+        res.json("Your fibonacci number is " + getFibNumber(fibIndex));
     });
+    
+}
+
+function getFibNumber(fibIndex){
+    let nextFibNumber = 1;
+    let currentFibNumber = 0;
+
+    for(let i=0; i<fibIndex; i++){
+        let tempNumber = nextFibNumber;
+        nextFibNumber = nextFibNumber + currentFibNumber;
+        currentFibNumber = tempNumber;
+    }
+
+    return currentFibNumber;
 }

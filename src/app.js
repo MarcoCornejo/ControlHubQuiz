666065fd4f33c800a4783533a70e91db052ef22f
Marco Cornejo <alejandroc_89@hotmail.com>
2021-03-19 15:37:34 -0700

    Added getFib endpoint that expects a fibIndex

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 
 module.exports = (app) => {
-    app.get("/", (req, res, next) => {
-        res.json('Hello there!');
+    app.get("/getFib/:fibIndex", (req, res, next) => {
+        res.json('Hello there, your entered number is: ' + req.params.fibIndex);
     });
 }

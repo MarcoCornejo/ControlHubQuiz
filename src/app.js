f382a7df46fc10943b5a48deffb168faa9f310a7
Marco Cornejo <alejandroc_89@hotmail.com>
2021-03-19 15:44:28 -0700

    Added validation that entered fibIndex is indeed a number

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 
 module.exports = (app) => {
     app.get("/getFib/:fibIndex", (req, res, next) => {
+        const fibIndex = parseInt(req.params.fibIndex);
+        if(isNaN(fibIndex)) { return res.json('Not a number') };
         res.json('Hello there, your entered number is: ' + req.params.fibIndex);
     });
 }
